9c9c388c29df25bfe3d63d7e8d4949989c2faa27
bizconfig <rakesh@users.noreply.github.com>
2018-12-10 00:14:03 +0530

    [10-12-2018] - Training session Day 1

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
-
-class App extends Component { 
-    state = {
-      persons: [
-        {
-          id:1,
-          name: 'raksh',
-          age: '30'
-        }, {
-          id:2,
-          name: 'filu',
-          age: 31
-        }
-      ]
-    } 
-    deleteHandler=(id)=>{
-     let persons=[...this.state.persons];
-     let personIndex=persons.findIndex(p=>{
-       return p.id === id;
-     })
-     persons.splice(personIndex,1);
-     this.setState({persons:persons});
-    } 
-    onChangeHandler=(event,id)=>{
-      let persons=[...this.state.persons];
-      let personIndex=persons.findIndex(p=>{
-        return p.id === id;
-      })
-      let person={...this.state.persons[personIndex]}
-      person.name=event.target.value;
-      persons[personIndex]=person;
-      this.setState({persons:persons});
-
-    }
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+class App extends Component {
   render() {
     return (
-      <div className="App">
-         <h1>This is headeing</h1>
-        { 
-          this.state.persons.map(person=>{
-            return <Person key={person.id} name={person.name} age={person.age} 
-            onDelete={()=>{this.deleteHandler(person.id)}} 
-            onChange={(event)=>this.onChangeHandler(event,person.id)}/>
-          })
-        }
+      <div>
+       <Layout>
+         <BurgerBuilder/>
+       </Layout>
       </div>
        )
   }
